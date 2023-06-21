7cd61930699e5af5949589608b002583a6be6d34
uzoefuna benson dominic <uzoefunam.benson@gmail.com>
2023-06-21 18:55:46 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import  { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import AboutUs from "../src/pages/AboutUs";
 import Home from "./pages/Home";
 import ContactUs from "./pages/ContactUs"
 import Services from "./pages/Services"
+import LoadingOverlay from './LoadingOverlay'; 
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   const routes = [
     { path: '/', component: <Home /> },
     { path: '/about', component: <AboutUs /> },
@@ -13,11 +25,14 @@ const App = () => {
   ];
 
   return (
-    <Routes className="font-sora " >
-      {routes.map(({ path, component }) => (
-        <Route key={path} path={path} element={component}  />
-      ))}
-    </Routes>
+    <>
+      {isLoading && <LoadingOverlay />} 
+      <Routes className="font-sora">
+        {routes.map(({ path, component }) => (
+          <Route key={path} path={path} element={component} />
+        ))}
+      </Routes>
+    </>
   );
 }
 
---
+++ src/LoadingOverlay.jsx
@@ -0,0 +1,15 @@
+
+const LoadingOverlay = () => {
+    return (
+        <div className="loading-overlay">
+        <div className="loading-content">
+          <div className="glowing-box"></div>
+          <div className="glowing-box"></div>
+          <div className="glowing-box"></div>
+          <div className="loading-text  font-sora ">Loading...</div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default LoadingOverlay;
