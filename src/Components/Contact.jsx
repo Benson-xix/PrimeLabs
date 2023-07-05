223c595106468de516ed0c2a646aa33acca6ea84
uzoefuna benson dominic <uzoefunam.benson@gmail.com>
2023-07-05 07:13:36 +0100

    Update Contact.jsx

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -126,7 +126,7 @@ const isEmailValid = (email) => {
             </div>
 
             <div className=" flex flex-col gap-6 w-[100%] md:justify-end mt-5 pr-[2rem] ">
-                <h5 className="text-[20px] text-Black font-bold leading-[120%] md:justify-end max-sm:justify-center font-sora flex ">Contact Information</h5>
+                <h5 className="text-[20px] text-Black font-bold leading-[120%] md:justify-center max-sm:justify-center font-sora flex ">Contact Information</h5>
                 <p className="font-300 leading-[120%] text-[18px] flex md:justify-end text-ash font-sora ">
                 Feel free to contact us for any inquiries or assistance.
                 <br className=""/>Our dedicated team is here to help you with prompt and reliable support.
